816805e304adc3f7cccb5591d9cabcc4a266926d
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 22:34:52 +0300

    task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,4 +6,4 @@ add module
 
 def add(a: int, b: int) -> int:
     ''' add to given numbers'''
-    return a+b
+    return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+concat func
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concat strings'''
+    return str1 + str2
