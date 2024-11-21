63bd9789c508d8affabca3de6b65bb9786683399
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 22:59:20 +0300

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+func sum_list
+'''
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    sum = 0.0
+    for ele in input_list:
+        sum += ele
+    return sum
