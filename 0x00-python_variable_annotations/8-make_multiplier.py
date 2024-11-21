e5124d8aa5bee925dd9d7f3cc32b990ef148ba65
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 23:52:22 +0300

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+
+'''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''return func that mul any given number by multiplier'''
+    def func(arg: float) -> float:
+        return multiplier * arg
+    return func
