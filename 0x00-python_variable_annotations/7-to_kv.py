acec4c3c2b799468b00470a6215c7e607bc1f8a4
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 23:34:41 +0300

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+to_key module
+'''
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''return tuple of string k and v square'''
+    com = (k, v ** 2)
+    return com
