bec20ed579491157bfc843420c22f5da4a120b28
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 22:49:31 +0300

    task 4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+some variavles annotaed with types
+'''
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
