96095168968e7e93bfb968d3be2f34c029d327c0
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 23:14:57 +0300

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+sum_mized_list this annotaed function should accept list of floats or intergers
+'''
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    '''take a list of int or floats and return the sum'''
+    acc = 0.0
+
+    for ele in mxd_lst:
+        acc += ele
+    return acc
