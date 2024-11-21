427f1f548315d0f21c4cda77c53a573110898032
mobashirr_wsl <zfzg223@gmail.com>
2024-11-21 22:46:07 +0300

    task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+to string module
+'''
+
+
+def to_str(n: float) -> str:
+    '''return string representaion of a float number'''
+    return str(n)
